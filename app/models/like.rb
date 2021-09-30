9b83e2b18979d9bde8ff0d41f59c557051b21a16
anonymous anonymous <patrick@firstdraft.com>
2021-09-30 21:06:06 +0000

    Validate uniqueness of likes.post_id

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,6 +5,8 @@ class Like < ApplicationRecord
 
   # Validations
 
+  validates :post_id, :uniqueness => { :scope => [:user_id] }
+
   validates :post_id, :presence => true
 
   validates :user_id, :presence => true
