f5184ee148622fd170614ac55b9082899137cda8
anonymous anonymous <patrick@firstdraft.com>
2021-09-30 21:07:10 +0000

    Direct association between UserResource and DirectMessageResource

--- app/resources/direct_message_resource.rb
+++ app/resources/direct_message_resource.rb
@@ -8,6 +8,9 @@ class DirectMessageResource < ApplicationResource
 
   # Direct associations
 
+  belongs_to :recipient,
+             resource: UserResource
+
   belongs_to :sender,
              resource: UserResource
 
--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -8,6 +8,10 @@ class UserResource < ApplicationResource
 
   # Direct associations
 
+  has_many   :recieved_messages,
+             resource: DirectMessageResource,
+             foreign_key: :recipient_id
+
   has_many   :liked_posts,
              resource: LikeResource
 
