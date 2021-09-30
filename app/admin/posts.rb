177752480243e2cf874f2013519b106dcad12a1c
anonymous anonymous <patrick@firstdraft.com>
2021-09-30 21:06:28 +0000

    Whitelist columns in Post for ActiveAdmin

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Post do
 
+ permit_params :user_id, :body, :reply_to_id, :repost_of_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
