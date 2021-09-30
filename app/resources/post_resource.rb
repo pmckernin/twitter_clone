39dcdf2e8b73334a10a6c49769f18285b8115410
anonymous anonymous <patrick@firstdraft.com>
2021-09-30 21:07:10 +0000

    Direct association between UserResource and PostResource

--- app/resources/post_resource.rb
+++ app/resources/post_resource.rb
@@ -9,6 +9,8 @@ class PostResource < ApplicationResource
 
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
 end
--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -8,6 +8,8 @@ class UserResource < ApplicationResource
 
   # Direct associations
 
+  has_many   :posts
+
   # Indirect associations
 
 end
