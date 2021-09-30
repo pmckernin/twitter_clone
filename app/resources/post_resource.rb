0265cf662669d4ca9038e7abf6a251e96255c1b6
anonymous anonymous <patrick@firstdraft.com>
2021-09-30 21:07:05 +0000

    Generate graphiti resources for posts

---
+++ app/resources/post_resource.rb
@@ -0,0 +1,14 @@
+class PostResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :user_id, :integer
+  attribute :body, :string
+  attribute :reply_to_id, :integer
+  attribute :repost_of_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
